44fc088c8092901e951af94497a00d79bf3c1465
Michael <7471919@naver.com>
2022-12-26 15:14:49 +0900

    feat: add post function on route /test

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('/test')
+  testPost(@Body() body): string {
+    body.code = 'success';
+    return body;
+  }
+
 }
